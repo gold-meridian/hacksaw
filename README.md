2b22c1ab4a5780c46d65e0b5a736f63065a611a8
Stephen T <xxlennygamerxx@gmail.com>
2025-09-01 22:44:07 -0500

    feat: set up basic repo structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# hacksaw
+
+---
+
+**hacksaw** is a HashLink VM implementation aiming to run HashLink binaries on .NET.
---
+++ src/Tomat.Hacksaw/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tomat.Hacksaw;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
